c1873d5c2c79391ebb0c958eb1c8ba6cb7e16f89
ElTruquini <d_olaya@hotmail.com>
2016-11-07 10:17:01 -0800

    Testing and performance cases completed

--- HeapArray.java
+++ HeapArray.java
@@ -2,7 +2,7 @@
  * University of Victoria
  * CSC 225 - Fall 2016
  * Assignment 3 - HeapSort
- * Creaated by - Daniel Olaya Nov 10th, 2016
+ * Creaated by - Daniel Olaya Nov 7th, 2016
 */
 import java.util.*;
 import java.lang.*;
@@ -14,7 +14,6 @@ class HeapArray{
 	protected int length;
 
 	//Create a new heap array using index 1 for root
-	//@param 
 	public HeapArray (int n){
 		length = n+1; //indexing starts at 1
 		this.hippieArray = new int [length]; 
@@ -22,6 +21,7 @@ class HeapArray{
 		hippieArray[counter] = 0; 
 	}
 
+	//generates n random numbers in a range (from low to high) and inserts them into the heap array.
 	private void ranGen (int n, int low, int high){
 		Random rand = new Random();
 		int i = 0;
@@ -33,22 +33,23 @@ class HeapArray{
 		}	
 	}
 
+	//returns number of elements in the heapArray
 	public int size (){
 		return counter;
 	}
 	
-	public int getMin (int i){
-		return hippieArray[i];
+	//returns min element which is at index 1
+	public int getMin (){
+		return hippieArray[1];
 	}
 
+	//Used to build a min Heaparray from an unsorted array
 	public int buildHeap (int[] arr, int i){
-		//System.out.println("BUILDHEAP with: " + hippieArray[i]);
 		int parent = (int)(Math.floor( (i)/2 ));
 		if (i <= 1){ 	// base case, i is root
 			return 0; 
 		}
 		if (arr[i] < arr[parent]){
-			//System.out.println("====Swaping: " +arr[i] + " with " + arr[parent]);
 			int temp = arr[parent];
 			arr[parent] = arr[i];
 			arr[i] = temp;
@@ -57,9 +58,8 @@ class HeapArray{
 		return -1; //this shall never be reached
 	}
 
-
+	// Fixes any violations on the heapArray after deletion of min element.
 	private void minHeapify (int i){
-		//System.out.println("MINHEAPIFY with:" + hippieArray[i]);
 		int left = 2*i;
 		int right = 2*i + 1;
 		int smallest = i;
@@ -78,9 +78,7 @@ class HeapArray{
 				}
 			} 
 		}
-
 		if (smallest != i){
-			//System.out.println("MINHEAPIFY SWAPPING: " +hippieArray[i] + " " + hippieArray[smallest]);
 			int temp = hippieArray[i];
 			hippieArray[i] = hippieArray[smallest];
 			hippieArray[smallest] = temp;
@@ -88,9 +86,9 @@ class HeapArray{
 		}
 	}
 
+	//deletes and returns the min element in the heapArray and corrects any violations in the heap after deletion of min element
 	public int deleteMin (){
 		int temp = hippieArray[1];
-		//System.out.println("Deleting Min: " + temp + ", counter: " + counter);
 		if (counter == 0){ //empty array
 			return 0;
 		}
@@ -102,7 +100,7 @@ class HeapArray{
 	}
 
 
-	//Inserts an integer into the heap and fixes any violations on Heap properties
+	//Inserts an integer into the heap using the min heap properties
 	public void insert (int value){
 		counter++;
 		if (counter == length){
@@ -134,8 +132,9 @@ class HeapArray{
 		System.out.println();
 	}
 
+	// doubles the size of the heapArray and copies al elements
 	public void growCopy (){
-		int [] temp = new int [counter * 2]; //doubles the size of the current array
+		int [] temp = new int [counter * 2]; 
 		for (int i = 0 ; i<counter ; i++){
 			temp[i] = hippieArray[i];
 		}
@@ -154,7 +153,6 @@ class HeapArray{
 
 	//helper method for heapSort
 	public void swap (int i, int j){
-			System.out.println("swapppppiiiiing " + hippieArray[i] +" with " + hippieArray[j]);
 			int temp = hippieArray[i];
 			hippieArray[i] = hippieArray[j];
 			hippieArray[j] = temp;
@@ -163,10 +161,12 @@ class HeapArray{
 	// Sorts in ascending order
 	public int heapSort(){
 		int [] sorted = new int [length];
+		int tempCounter = counter;
 		for (int i = 1; i < length ; i++){
 			sorted[i]=deleteMin();
 		}
 		hippieArray = sorted;
+		counter = tempCounter; //restores the counter 
 		return 0;
 	}
 
@@ -175,6 +175,10 @@ class HeapArray{
 	//TEST 3: Testing makeHeap method with a reverse sorted array into, uses growCopy method to double array size
 	//TEST 4: Testing makeHeap method with a sorted array into, uses growCopy method to double array size
 	//TEST 5: Testing makeHeap method with a unsorted array, uses growCopy method to double array size 
+	//TEST 6: Testing delMin with empty array
+	//TEST 7: Testing growCopy, size, deleteMin and minHeapify methods
+	//TEST 8: Testing heapsort, must return an array sorted in ascending order.
+	//TEst 9, 10, 11, 12, 13 : Performance tests, check terminal for results.
 	public void test (int test){
 		try{
 			report(test);
@@ -185,12 +189,10 @@ class HeapArray{
 		}
 	}
 
+	//Writes test results into pq_test.txt file.
 	public void report(int test) throws IOException {
-
 			FileWriter file = new FileWriter("pq_test.txt", true);
 			BufferedWriter writer = new BufferedWriter (file);
-
-
 			switch (test){
 				case 0:
 					writer.append("\n---+++ Integer input +++--- \n");
@@ -215,26 +217,29 @@ class HeapArray{
 					writer.append("\nTEST 5: Testing makeHeap method with a unsorted array, uses growCopy method to double array size ");	
 					writer.append("\nOutput: \n");
 					break;
+				case 6:
+					writer.append("\nTEST 6: TEST 6: Testing delMin with empty array");	
+					writer.append("\nOutput: \n");
+					break;
+				case 7:
+					writer.append("\nTEST 7: Testing growCopy, size, deleteMin and minHeapify methods");	
+					writer.append("\nArray after deleting MIN: \n");
+					break;	
+				case 8:
+					writer.append("\nTEST 8: Testing heapsort, must return an array sorted in ascending order.");	
+					writer.append("\nArray after sorting in ascending order: \n");
+					break;
 
 			}
-
-				for (int i : hippieArray){
-					writer.append( i +" ");
-
-					}
-					writer.append("\n");
-					
-				
-
-			//file.close();
+			for (int i : hippieArray){
+				writer.append( i +" ");
+			}
+			writer.append("\nThe size of the heapArray is: " + counter + "\n");	
 			writer.close();
-
-
-		
 	}
 
+	//All test cases devoleped in main. To test just remove "/*" at the beginning of each test case code section.
 	public static void main(String[] args) {
-		
 
 	// TEST1: Create a heap with 10 random ints with a range from 0 to 1000
 			/*
@@ -298,8 +303,7 @@ class HeapArray{
 			System.out.print("makeHeap result: ");
 			unsorted.print();
 			unsorted.test(4);
-
-		//*/
+			//*/
 
 	//	TEST 5: Testing makeHeap method with a unsorted array, uses growCopy method to double array size
 			/*
@@ -318,96 +322,155 @@ class HeapArray{
 			System.out.print("makeHeap result: ");
 			unsorted.print();
 			unsorted.test(5);
-		//*/
+			//*/
 
+	//	TEST 6: Testing delMin with empty array
+			/*
+			int n = 10;
+			HeapArray testy = new HeapArray (n);
+			testy.deleteMin();
+			System.out.print("Initial: ");
+			testy.test(0);
+			testy.print();
+			testy.deleteMin();
+			System.out.print("Final: ");
+			testy.print(); 
+			testy.test(6);
+			//*/
 
+	//	TEST 7: Testing growCopy, size, deleteMin and minHeapify methods
+			/*
+			int n = 10;
+			HeapArray testy = new HeapArray (n);
+			testy.insert (4);
+			testy.insert (3);
+			testy.insert (2);
+			testy.insert (1);
+			testy.insert (9);
+			testy.insert (3);
+			System.out.print("Initial: ");
+			testy.print();
+			testy.test(0);
+			System.out.println("Size " + testy.size());  //must return 6
+			testy.deleteMin();
+			testy.test(7);
+			System.out.println("Size: " + testy.size()); //must return 5
+			testy.deleteMin();
+			testy.test(7);
+			System.out.println("Size: " + testy.size()); //must return 4
+			testy.deleteMin();
+			testy.test(7);
+			System.out.println("Size: " + testy.size()); //must return 3
+			testy.deleteMin();
+			testy.test(7);
+			System.out.println("Size: " + testy.size()); //must return 2
+			testy.deleteMin();
+			testy.test(7);		
+			System.out.println("Size: " + testy.size()); //must return 1
+			testy.deleteMin();
+			testy.test(7);	
+			System.out.println("Size: " + testy.size()); //must return 0
+			System.out.print("FINAL: ");
+			testy.print();
+			//*/
 
+	//	TEST 8: Testing heapsort, must return an array sorted in ascending order. Change n to modify size of heapArray
+			/*
+			int n = 200;
+			int hiRange = 1000;
+			HeapArray testy = new HeapArray (n);
+			testy.ranGen(n, 0 ,hiRange);
+			System.out.print("Main FINAL: ");
+			System.out.print("Initial heapArray: ");
+			testy.test(0);
+			testy.print();
+			System.out.println("PRE SORT Counter is: " +testy.size() );
+			System.out.println();
+			testy.heapSort();
+			testy.test(8);
+			System.out.print("FINAL Sorted Array - ");
+			testy.print();
+			System.out.println("Counter is: " +testy.size() );
+			//*/
 
-		//	testing heapsort
-		/*
-		testy.insert (4);
-		testy.insert (1700);
-		testy.insert (1);
-		testy.insert (5);
-		testy.insert (3);
-		testy.insert (2);
-		testy.insert (10);
-		testy.insert (19999);
-		testy.insert (7);
-		testy.insert (30);
-		testy.insert (6);
-		//testy.insertUnsorted (3);
-		System.out.print("Initial: ");
-		testy.print();
-		System.out.println();
+	//TEST 9: Testing performance n=100 with random generated ints with a range from 0 to 1,000
+			/*
+			long startTime = System.currentTimeMillis();
+			int n = 100;
+			int hiRange = 1000;
+			HeapArray testy = new HeapArray (n);
+			testy.ranGen(n, 0 ,hiRange);
+			System.out.print("Main FINAL: ");
+			testy.print();
+			System.out.println();
+			testy.test(1);
+			long endTime = System.currentTimeMillis();
+			double totalTimeSeconds = (endTime - startTime) / 1000.0;
+			System.out.printf("Total Time (seconds): %.2f\n", totalTimeSeconds);
+			//*/
 
-		testy.heapSort();
+	//TEST 10: Testing performance n=1000 with random generated ints with a range from 0 to 1,000
+			/*
+			long startTime = System.currentTimeMillis();
+			int n = 1000;
+			int hiRange = 1000;
+			HeapArray testy = new HeapArray (n);
+			testy.ranGen(n, 0 ,hiRange);
+			System.out.print("Main FINAL: ");
+			testy.print();
+			System.out.println();
+			testy.test(1);
+			long endTime = System.currentTimeMillis();
+			double totalTimeSeconds = (endTime - startTime) / 1000.0;
+			System.out.printf("Total Time (seconds): %.2f\n", totalTimeSeconds);
+			//*/
 
-		System.out.print("FINAL: ");
-		testy.print();
-		
+	//TEST 11: Testing performance n=10,000 with random generated ints with a range from 0 to 1,000
+			/*
+			long startTime = System.currentTimeMillis();
+			int n = 10000;
+			int hiRange = 1000;
+			HeapArray testy = new HeapArray (n);
+			testy.ranGen(n, 0 ,hiRange);
+			System.out.print("Main FINAL: ");
+			testy.print();
+			System.out.println();
+			testy.test(1);
+			long endTime = System.currentTimeMillis();
+			double totalTimeSeconds = (endTime - startTime) / 1000.0;
+			System.out.printf("Total Time (seconds): %.2f\n", totalTimeSeconds);
+			//*/
+	
+	//TEST 12: Testing performance n=100,000 with random generated ints with a range from 0 to 1,000
+			/*
+			long startTime = System.currentTimeMillis();
+			int n = 100000;
+			int hiRange = 1000;
+			HeapArray testy = new HeapArray (n);
+			testy.ranGen(n, 0 ,hiRange);
+			System.out.print("Main FINAL: ");
+			testy.print();
+			System.out.println();
+			testy.test(1);
+			long endTime = System.currentTimeMillis();
+			double totalTimeSeconds = (endTime - startTime) / 1000.0;
+			System.out.printf("Total Time (seconds): %.2f\n", totalTimeSeconds);
+			//*/
 
-		
-
-		//	growCopy, size and minHeapify
-		/*
-		testy.insert (4);
-		testy.insert (3);
-		testy.insert (2);
-		testy.insert (1);
-		testy.insert (9);
-		testy.insert (3);
-		System.out.print("Initial: ");
-		testy.print();
-		//System.out.println("Size " + testy.size());
-		//testy.deleteMin();
-		//System.out.println("Size: " + testy.size()); //must return 0
-		//testy.insert (5);
-		//System.out.println("Size: " + testy.size()); //must return 1
-		//testy.insert (0);
-		//System.out.println("Size: " + testy.size()); //must return 8
-		System.out.print("FINAL: ");
-		testy.print();
-		*/
-
-		//	 delMin with empty array
-		/*
-		testy.deleteMin();
-		System.out.print("Initial: ");
-		testy.print();
-		testy.deleteMin();
-		testy.print(); 
-		*/
-
-		// 	testing delMin 
-		/*
-		testy.insert (5);
-		testy.insert (4);
-		testy.insert (3);
-		testy.insert (2);
-		testy.insert (8);
-		testy.insert (6);
-		testy.insert (1);
-		System.out.print("Initial: ");
-		testy.print();
-		testy.deleteMin();
-		testy.deleteMin();
-		testy.deleteMin();
-		testy.print(); 
-		*/
-
-		//	testing delMin 1 element and size
-		/*
-		testy.insert (5);
-		System.out.print("Initial: ");
-		testy.print();
-		System.out.println("Size " + testy.size()); //must return 1
-		testy.deleteMin();
-		System.out.println("Size: " + testy.size()); //must return 0
-		testy.insert (4);
-		System.out.println("Size: " + testy.size()); //must return 1
-		System.out.print("FINAL: ");
-		testy.print(); //only 4
-		*/
+	//TEST 13: Testing performance n=1,000,000 with random generated ints with a range from 0 to 1,000
+			/*
+			long startTime = System.currentTimeMillis();
+			int n = 1000000;
+			int hiRange = 1000;
+			HeapArray testy = new HeapArray (n);
+			testy.ranGen(n, 0 ,hiRange);
+			System.out.print("Main FINAL: ");
+			testy.print();
+			System.out.println();
+			testy.test(1);
+			long endTime = System.currentTimeMillis();
+			double totalTimeSeconds = (endTime - startTime) / 1000.0;
+			System.out.printf("Total Time (seconds): %.2f\n", totalTimeSeconds);
+			//*/
 	}
 }
